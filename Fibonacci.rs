345cc3f9a75fc139633c600bba0f42b52df63977
Frank <33494411+frqnk@users.noreply.github.com>
2023-06-04 18:23:55 -0300

    espaço adicionado entre os resultados na saída

--- Fibonacci.rs
+++ Fibonacci.rs
@@ -2,6 +2,6 @@ fn main(){
     let root = 5_f32.sqrt();
     let phi = (1.0 + root) / 2.0;
     for n in 0..16 {
-        print!("{}", (phi.powi(n) / root).round()); 
+        print!("{} ", (phi.powi(n) / root).round()); 
     }
 }
